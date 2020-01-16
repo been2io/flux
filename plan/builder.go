f0671fc941e3f4d58088e3163cebe8051765d632
ben <ben>
2020-01-16 14:43:12 +0800

    enable stage

--- plan/builder.go
+++ plan/builder.go
@@ -36,10 +36,10 @@ type planner struct {
 }
 
 func (p *planner) Plan(fspec *flux.Spec) (*Spec, error) {
-	/*fspec, err := StagePlanner{}.Plan(fspec)
+	fspec, err := StagePlanner{}.Plan(fspec)
 	if err != nil {
 		return nil, err
-	}*/
+	}
 	ip, err := p.lp.CreateInitialPlan(fspec)
 	if err != nil {
 		return nil, err
