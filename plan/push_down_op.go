9fd5c94e7cf766b55a157283f1c81c76a4f9bb5b
ben.liao <ben.liao@bytedance.com>
2020-04-03 15:25:20 +0800

    push down limited op

--- plan/push_down_op.go
+++ plan/push_down_op.go
@@ -5,20 +5,22 @@ import (
 )
 
 func IsPushDownOp(o *flux.Operation) bool {
-
 	switch o.Spec.Kind() {
 	case "range":
+		return true
 	case "filter":
+		return true
 	case "window":
 	case "first":
 	case "last":
 	case "sum":
 	case "sample":
 	case "group":
+		return true
 	case "from":
 	case "influxDBFrom":
 	default:
 		return false
 	}
-	return true
+	return false
 }
